a284f6c94f1d5cbf925aaa047a46a056334df9c3
Vipin Vijayan <vvjnvt@gmail.com>
2021-05-28 13:20:12 -0400

    sh to bash

--- prepare-wmt18-text.sh
+++ prepare-wmt18-text.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh ./scripts/task1-tokenize.sh
-sh ./scripts/task1-tok-stats.sh
-sh ./scripts/task1-bpe.sh -m 10000
+bash ./scripts/task1-tokenize.sh
+bash ./scripts/task1-tok-stats.sh
+bash ./scripts/task1-bpe.sh -m 10000
 cp -R data/task1/image_splits task1-data/
